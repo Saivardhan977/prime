e0ef4f570d0fbca0ef0b22d7240ea1ccb58fb679
Saivardhan977 <saivardhang00@gmail.com>
2024-12-10 11:53:51 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,15 +1,15 @@
 public class Prime {
     public static void main(String[] args) {
 
+       
         for (int num = 2; num <= 100; num++) {
             boolean isPrime = true;
             for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
+                if (num % i == 0) { 
                     isPrime = false;
                     break;
                 }
             }
-
             if (isPrime) {
                 System.out.println(num);
             }
